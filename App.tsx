3b7a8e6f1a08ddd3a6d7e581742a3b96b5cae7dc
Milena <milena@wapstore.com.br>
2024-04-09 21:33:33 -0300

    chore: add config development builds

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className='font-rubikRegular text-sm color-pink'>Open up App.tsx to start working on your app!</Text>
+      <Text className='font-rubikMedium text-sm color-pink'>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
